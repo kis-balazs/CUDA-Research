8b2ebac465cd4c24b79f08935b1c789cad9d9dfb
Balazs Kis <balazs_kis35@yahoo.com>
2024-12-07 22:31:35 +0000

    create functions for cuBLAS and cuBLASLt comparison

--- 06_CUDA_APIs/01_cuBLAS/03_compare_cuBLAS_cuBLASLt.cu
+++ 06_CUDA_APIs/01_cuBLAS/03_compare_cuBLAS_cuBLASLt.cu
@@ -2,3 +2,65 @@
 #include <cublas_v2.h>
 #include <cublasLt.h>
 #include <cuda_fp16.h>
+#include <functional>
+#include <vector>
+#include <numeric>
+
+
+#define N 2048
+#define M 1024
+#define K 2048
+
+// CHECK_CUDA_ERROR definition for detailed logging
+
+void initMat(float *MAT, int r, int c) {
+    for (int i = 0; i < r * c; i++) MAT[i] = (float)rand() / RAND_MAX;
+}
+
+// CUDA event-based timing function
+float timeKernel(std::function<void()> kernel) {
+	cudaEvent_t start, stop;
+	float time;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaEventRecord(start);
+	kernel();
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+
+	cudaEventElapsedTime(&time, start, stop);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	return time;
+}
+
+float benchmarkKernel(std::function<void()> kernel, int runsWarmup, int runsBenchmark) {
+	for (int i = 0; i < runsWarmup; i++) kernel();
+
+	std::vector<float> times;
+	for (int i = 0; i < runsBenchmark; i++) times.push_back(timeKernel(kernel));
+
+	return std::accumulate(times.begin(), times.end(), 0.0f) / runsBenchmark;
+}
+
+
+__global__ void mulMatsGpu(float *A, float *B, float *C, int n, int m, int k) {
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
+	int col = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (row < n && col < k) {
+		float sum = 0;
+		for (int l = 0; l < m; l++)
+			sum += A[row * m + l] * B[l * k + col];
+		C[row * k + col] = sum;
+	}
+}
+
+
+int main() {
+	return 0;
+}
