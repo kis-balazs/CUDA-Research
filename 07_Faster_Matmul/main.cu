aab3205b8a7809d4e3d2a57844e55e1861657631
Balazs Kis <bkis@nvidia.com>
2025-04-15 16:53:18 +0200

    update flow

--- 07_Faster_Matmul/main.cu
+++ 07_Faster_Matmul/main.cu
@@ -6,16 +6,17 @@ void initMat(float *MAT, int r, int c) {
 
 
 void mulMatsCpu(float *A, float *B, float *C, int n, int m, int k) {
-	float sum;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < k; j++) {
-			sum = 0;
-			for (int l = 0; l < m; l++)
-				sum += A[i * m + l] * B[l * k + j];
-			C[i * k + j] = sum;
-		}
+    float sum;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            sum = 0;
+	    for (int l = 0; l < m; l++)
+	        sum += A[i * m + l] * B[l * k + j];
+	    C[i * k + j] = sum;
 	}
+    }
 }
+
 
 int main(int argc, char **argv) {
     if (argc < 2 || argc > 2) {
@@ -34,50 +35,50 @@ int main(int argc, char **argv) {
         pcdi();
         return 0;
     }
+    auto cudaFunction = cudaFunctionMap.find(argv[1]);
+    if (cudaFunction != cudaFunctionMap.end()) {
+	int sizeA, sizeB, sizeC;
 
-    float *hA, *hB, *hC, *hC_ref;
-    float *dA, *dB, *dC, *dC_ref;
+        float *hA, *hB, *hC, *hC_ref;
+        float *dA, *dB, *dC, *dC_ref;
 
-    int sizes[5] = {64, 128, 256, 512, 1024};
-    for (auto s: sizes) {
-        int N = s, M = s, K = s;
+        int sizes[5] = {256, 512, 1024, 2048, 4096};
+        for (auto s: sizes) {
+            int N = s, M = s, K = s;
 
-        // ------
-        int sizeA = N * M * sizeof(float);
-        int sizeB = M * K * sizeof(float);
-        int sizeC = N * K * sizeof(float);
+            sizeA = N * M * sizeof(float);
+            sizeB = M * K * sizeof(float);
+            sizeC = N * K * sizeof(float);
 
-        hA = (float*)malloc(sizeA);
-        hB = (float*)malloc(sizeB);
-        hC = (float*)malloc(sizeC);
-        hC_ref = (float*)malloc(sizeC);
+            hA = (float*)malloc(sizeA);
+            hB = (float*)malloc(sizeB);
+            hC = (float*)malloc(sizeC);
+            hC_ref = (float*)malloc(sizeC);
 
-        srand(time(NULL));
-        initMat(hA, N, M);
-        initMat(hB, M, K);
+            srand(time(NULL));
+            initMat(hA, N, M);
+            initMat(hB, M, K);
 
-        cudaMalloc(&dA, sizeA);
-        cudaMalloc(&dB, sizeB);
-        cudaMalloc(&dC, sizeC);
-        cudaMalloc(&dC_ref, sizeC);
+            cudaMalloc(&dA, sizeA);
+            cudaMalloc(&dB, sizeB);
+            cudaMalloc(&dC, sizeC);
+            cudaMalloc(&dC_ref, sizeC);
 
-        cudaMemcpy(dA, hA, sizeA, cudaMemcpyHostToDevice);
-        cudaMemcpy(dB, hB, sizeB, cudaMemcpyHostToDevice);
+            cudaMemcpy(dA, hA, sizeA, cudaMemcpyHostToDevice);
+            cudaMemcpy(dB, hB, sizeB, cudaMemcpyHostToDevice);
 
-        // execute based on user input
-        float benchmarkTime;
-        float alpha = 1.0f, beta = 0.0f;
-        auto it = cudaFunctionMap.find(argv[1]);
-        if (it != cudaFunctionMap.end()) {
+	    // -----
+            float benchmarkTime;
+            float alpha = 1.0f, beta = 0.0f;
             
             // TODO replace with cuBLAS implementation -> the benchmark!
             mulMatsCpu(hA, hB, hC_ref, N, M, K);
-            //refTime = it->second(N, M, K, alpha, dA, dB, beta, dC_ref, false);
+            //refTime = cudaFunction->second(N, M, K, alpha, dA, dB, beta, dC_ref, false);
 
-            benchmarkTime = it->second(N, M, K, alpha, dA, dB, beta, dC, true);
+            benchmarkTime = cudaFunction->second(N, M, K, alpha, dA, dB, beta, dC, true);
             cudaDeviceSynchronize();
             cudaError_t err = cudaGetLastError();
-            if (err != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(err));
+            if (err != cudaSuccess) printf("> CUDAError: %s\n", cudaGetErrorString(err));
 
             // cudaMemcpy(hC_ref, dC_ref, sizeC, cudaMemcpyDeviceToHost);
             cudaMemcpy(hC, dC, sizeC, cudaMemcpyDeviceToHost);
@@ -88,11 +89,11 @@ int main(int argc, char **argv) {
                 std::cout << "SIZE: " << N <<  "; average elapsed time: " << benchmarkTime <<
                 "s; performance: " << (flops * 1e-9) / benchmarkTime << " GFLOPS" << std::endl;
             }
-        } else {
-            std::cerr << "Entry not found!" << std::endl;
-        }
-        free(hA); free(hB); free(hC); free(hC_ref);
+	}
+	free(hA); free(hB); free(hC); free(hC_ref);
         cudaFree(dA); cudaFree(dB); cudaFree(dC); cudaFree(dC_ref);
+    } else {
+        std::cerr << "Entry not found!" << std::endl;
     }
     return 0;
 }
--- 07_Faster_Matmul/main.cuh
+++ 07_Faster_Matmul/main.cuh
@@ -18,33 +18,57 @@ using cudaFunctionMap_t = std::map<
 
 
 int div_ceil(int numerator, int denominator) {
-  std::div_t res = std::div(numerator, denominator);
-  return res.rem ? (res.quot + 1) : res.quot;
+    std::div_t res = std::div(numerator, denominator);
+    return res.rem ? (res.quot + 1) : res.quot;
 }
 
 bool verify_matrix(float *matRef, float *matOut, int N) {
-  for (int i = 0; i < N; i++) {
-    if (std::fabs(matRef[i] - matOut[i]) > 0.01) return false;
-  }
-  return true;
+    for (int i = 0; i < N; i++) {
+        if (std::fabs(matRef[i] - matOut[i]) > 0.01) {
+	    std::cout << matRef[i] << " " << matOut[i] << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
 
 // include SGEMM implementations
 #include "01_Naive.cuh"
-float run_SGEMMNaive(int N, int M, int K, float alpha, float *A, float *B,
+float runSgemmNaive(int N, int M, int K, float alpha, float *A, float *B,
                      float beta, float *C, bool benchmark) {
     dim3 gridDim(div_ceil(N, BLOCK_SIZE), div_ceil(K, BLOCK_SIZE));
     dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
 
     if (benchmark)
         return benchmarkKernel([&]() {
-            sgemm_naive<<<gridDim, blockDim>>>(N, M, K, alpha, A, B, beta, C);
+            sgemmNaive<<<gridDim, blockDim>>>(N, M, K, alpha, A, B, beta, C);
         }, WARMUP_RUNS, BENCHMARK_RUNS) / 1000.0f;  // seconds
-    else
-        sgemm_naive<<<gridDim, blockDim>>>(N, M, K, alpha, A, B, beta, C);
+    else {
+        sgemmNaive<<<gridDim, blockDim>>>(N, M, K, alpha, A, B, beta, C);
         return -1.0f;
+    }
 }
 
+#include "02_GlobalMemCoalesce.cuh"
+float runSgemmGlobalMemCoalesce(int N, int M, int K, float alpha, float *A, float *B,
+				float beta, float *C, bool benchmark) {
+    dim3 gridDim(div_ceil(N, BLOCK_SIZE), div_ceil(K, BLOCK_SIZE));
+    dim3 blockDim(BLOCK_SIZE * BLOCK_SIZE); // 1d layout, warp-tiling calculated in-kernel!
+
+    if (benchmark)
+        return benchmarkKernel([&]() {
+	    sgemmGlobalMemCoalesce<BLOCK_SIZE>
+	        <<<gridDim, blockDim>>>(N, M, K, alpha, A, B, beta, C);
+	}, WARMUP_RUNS, BENCHMARK_RUNS) / 1000.0f;  // seconds
+    else {
+	sgemmGlobalMemCoalesce<BLOCK_SIZE>
+	    <<<gridDim, blockDim>>>(N, M, K, alpha, A, B, beta, C);
+	return -1.0;
+    }
+}
+
+
 cudaFunctionMap_t cudaFunctionMap = {
-    {"run_SGEMMNaive", run_SGEMMNaive}
+    {"runSgemmNaive", runSgemmNaive},
+    {"runSgemmGlobalMemCoalesce", runSgemmGlobalMemCoalesce}
 };
